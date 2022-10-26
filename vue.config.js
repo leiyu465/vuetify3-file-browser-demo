36facf0bedd1afa42553acce7f0245d98dbdb8b0
gaoyuan1 <gaoyuan1@sensetime.com>
2022-10-26 11:35:44 +0800

    first commit for vuetify3 demo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path")
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack:{
+      resolve:{
+          alias:{
+             vue: path.resolve('./node_modules/vue')
+          }
+      }
+  },
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  }
 })
